d8da5bf9251047537616482282f6dba6224af319
William Kong <william@vmfarms.com>
2017-08-29 13:32:58 -0400

    updated agnear.fish

--- .config/fish/functions/agnear.fish
+++ .config/fish/functions/agnear.fish
@@ -1,7 +1,12 @@
-function agnear --description "agnear '\bPATTERN1\W+(\w+\W+){1,n}PATTERN2'"
+function agnear --description "agnear '\bPATTERN1\W+(\w+\W+){1,count}PATTERN2'"
     if [ (count $argv) -eq 3 ]  # [] is shorthand for test command
-        ag '\\b'$argv[1]'\\W+(\\w+\\W+){1,'$argv[3]'}'$argv[2]
-    else
+        if [ $argv[3] -eq $argv[3] ]  # test whether argument is integer
+            ag '\\b'$argv[1]'\\W+(\\w+\\W+){1,'$argv[3]'}'$argv[2]
+        end
+    else if [ (count $argv) -eq 2 ]
         ag '\\b'$argv[1]'\\W+(\\w+\\W+){1,10}'$argv[2]
+    else
+        echo 'usage: '
+        echo 'agnear PATTERN1 PATTERN2 [count]'
     end
 end
