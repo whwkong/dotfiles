4adbb22b0cd5bb8ae03365f63f4d8f85be76d676
William Kong <coreloop09@gmail.com>
2018-12-25 09:40:07 -0500

    update .pdbrc, .pdbrc.py

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,9 +1,9 @@
 import pdb
 
-# specifically for pdbpp
+
 class Config(pdb.DefaultConfig):
     sticky_by_default = True
+    current_line_color = 44
 
     def setup(self, pdb):
-        from pprint import pprint
-        import json
+        pass
