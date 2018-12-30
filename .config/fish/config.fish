19481159e343e55ec3f40063e03debda09f1e5ff
William Kong <coreloop09@gmail.com>
2018-12-30 11:37:53 -0500

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -64,6 +64,14 @@ if status --is-login
         status --is-interactive; and source (rbenv init -|psub)
     end
 
+    # chruby
+    if test -e /usr/local/share/chruby/chruby.fish
+        source /usr/local/share/chruby/chruby.fish
+        # only for auto-switching ruby versions when changing dirs
+        # see: https://github.com/postmodern/chruby#auto-switching
+        source /usr/local/share/chruby/auto.fish
+    end
+
     # Ruby exports
     set -gx GEM_HOME $HOME/.gems
 
