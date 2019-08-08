413e115e1610d27f19c5f10d82e08f3e9fcef905
William Kong <william@bht.ai>
2019-08-08 14:04:35 -0400

    update agnear

--- .config/fish/functions/agnear.fish
+++ .config/fish/functions/agnear.fish
@@ -1,10 +1,10 @@
 function agnear --description "agnear '\bPATTERN1\W+(\w+\W+){1,count}PATTERN2'"
-    if [ (count $argv) -eq 3 ]  # [] is shorthand for test command
+    if [ (count $argv) -eq 3 ]
         if [ $argv[3] -eq $argv[3] ]  # test whether argument is integer
-            ag '\\b'$argv[1]'\\W+(\\w+\\W+){1,'$argv[3]'}'$argv[2]
+            ag -i '\\b'$argv[1]'\\W+(\\w+\\W+){1,'$argv[3]'}'$argv[2]
         end
     else if [ (count $argv) -eq 2 ]
-        ag '\\b'$argv[1]'\\W+(\\w+\\W+){1,10}'$argv[2]
+        ag -i '\\b'$argv[1]'\\W+(\\w+\\W+){1,10}'$argv[2]
     else
         echo 'usage: '
         echo 'agnear PATTERN1 PATTERN2 [count]'
