d9d246846aea7a150a835bec15dcb51a1236ccd5
William Kong <william@bht.ai>
2020-01-10 11:26:45 -0500

    update agnear

--- .config/fish/functions/agnear.fish
+++ .config/fish/functions/agnear.fish
@@ -6,7 +6,9 @@ function agnear --description "agnear '\bPATTERN1\W+(\w+\W+){1,count}PATTERN2'"
     else if [ (count $argv) -eq 2 ]
         ag -i '\\b'$argv[1]'\\W+(\\w+\\W+){1,10}'$argv[2]
     else
+        echo 'searches for 2 patterns near each other.'
         echo 'usage: '
         echo 'agnear PATTERN1 PATTERN2 [count]'
+        echo '  count defaults to 10'
     end
 end
