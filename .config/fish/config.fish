2dab757c6f85b900df1a5c83f96409a641bafb3a
William Kong <coreloop09@gmail.com>
2020-05-19 15:20:43 -0400

    remove virtualfish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -109,10 +109,6 @@ set -x PATH $HOME/.local/bin $PATH
 # set python version to version pointed to by pyenv
 set -gx VIRTUALFISH_DEFAULT_PYTHON (which -p python3)
 
-# activate plugins
-# http://virtualfish.readthedocs.io/en/latest/plugins.html#auto-activation
-eval (python3 -m virtualfish auto_activation projects)
-
 # bootstrap fisher
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
