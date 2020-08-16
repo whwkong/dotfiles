3e1e9a3f19f88e0c2983b136cfbc92154ff692cd
William Kong <william@bht.ai>
2020-08-16 19:44:02 -0400

    from ubuntu 20.4 upgrade; add new fish stuff

---
+++ .config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,6 @@
+set -g VIRTUALFISH_VERSION 2.4.0
+set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python3
+source /home/william/.local/lib/python3.8/site-packages/virtualfish/virtual.fish
+source /home/william/.local/lib/python3.8/site-packages/virtualfish/environment.fish
+source /home/william/.local/lib/python3.8/site-packages/virtualfish/auto_activation.fish
+emit virtualfish_did_setup_plugins
---
+++ .config/fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/william/.fzf/shell/key-bindings.fish
