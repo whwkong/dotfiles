de04e2d75a1db66d22109659e6659a80df7dac54
William Kong <william@bht.ai>
2020-08-16 12:20:21 -0400

    update

--- dev/setup/README.md
+++ dev/setup/README.md
@@ -1,1 +1,7 @@
 # README
+
+sudo apt install vim -y
+install pip
+
+install vim plugins
+
---
+++ dev/setup/setup_ubuntu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+
+set -euxo pipefail
+
+sudo apt update
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt install vim python3-pip silversearcher-ag fish -y
+
+
+# python2 is not installed by default on 20.04
+
+pip3 install powerline-shell
+pip3 install --user virtualfish
+
