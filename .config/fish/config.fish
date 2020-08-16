dc7929375a88ffc0d3de90bfbb7c7b8f119e789c
William Kong <william@bht.ai>
2020-08-16 19:39:37 -0400

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -141,3 +141,21 @@ end
 # Custom plugins may be added to ~/.tacklebox/plugins/
 # Example format: set tacklebox_plugins python extract
 
+
+# Paths to your tackle
+set tacklebox_path ~/.tackle ~/.tacklebox
+
+# Theme
+#set tacklebox_theme entropy
+
+# Which modules would you like to load? (modules can be found in ~/.tackle/modules/*)
+# Custom modules may be added to ~/.tacklebox/modules/
+# Example format: set tacklebox_modules virtualfish virtualhooks
+
+# Which plugins would you like to enable? (plugins can be found in ~/.tackle/plugins/*)
+# Custom plugins may be added to ~/.tacklebox/plugins/
+# Example format: set tacklebox_plugins python extract
+
+# Load Tacklebox configuration
+. ~/.tacklebox/tacklebox.fish
+
--- .config/setup/setup_ubuntu.sh
+++ .config/setup/setup_ubuntu.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/bash
-
+#!/usr/bin/env bash
 
 set -euxo pipefail
 
 sudo apt update
 sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt install vim python3-pip silversearcher-ag fish -y
-
+sudo add-apt-repository ppa:nathan-renniewaldock/flux
+sudo apt install vim python3-pip silversearcher-ag fish curl httpie jq fluxgui -y
+# i3 specifics
+sudo apt install i3 rofi scrot
 
 # python2 is not installed by default on 20.04
 
 pip3 install powerline-shell
 pip3 install --user virtualfish
-
