6e2d61cf422a9a931ab70afd6a72155646867335
William Kong <william@bht.ai>
2020-08-22 18:32:41 -0400

    renamed:    setup_ubuntu.sh -> setup_ubuntu_basic.sh

--- .config/setup/setup_ubuntu_basic.sh
+++ .config/setup/setup_ubuntu_basic.sh
@@ -4,8 +4,9 @@ set -euxo pipefail
 
 sudo apt update
 sudo apt-add-repository ppa:fish-shell/release-3
-sudo add-apt-repository ppa:nathan-renniewaldock/flux
+sudo add-apt-repository ppa:nathan-renniewaldock/flux snapd
 sudo apt install vim python3-pip silversearcher-ag grc fish curl httpie jq fluxgui -y
+
 # i3 specifics
 sudo apt install i3 rofi scrot
 
@@ -13,3 +14,4 @@ sudo apt install i3 rofi scrot
 
 pip3 install powerline-shell
 pip3 install --user virtualfish
+
