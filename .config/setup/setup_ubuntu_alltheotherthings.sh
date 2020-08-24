1cc6ef8943a47b8bdcaf3d6e2c4b16fc4125987c
William Kong <william@bht.ai>
2020-08-24 12:38:53 -0400

    update setup scripts

--- .config/setup/setup_ubuntu_alltheotherthings.sh
+++ .config/setup/setup_ubuntu_alltheotherthings.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 sudo apt update
 sudo add-apt-repository ppa:nathan-renniewaldock/flux 
-sudo apt install fluxgui -y
+sudo apt install fluxgui nodejs npm -y
 
 # i3 specifics
 sudo apt install i3 rofi scrot
