b0b1df2b511311a2141f9cb7d2a7e33bf2bbde27
William Kong <william@bht.ai>
2020-12-11 18:23:08 -0500

    update agnear for commutivity

--- .config/fish/functions/agnear.fish
+++ .config/fish/functions/agnear.fish
@@ -2,10 +2,10 @@ function agnear --description "agnear 'PATTERN1.(1,count}PATTERN2'"
     # \b = word boundary
     if [ (count $argv) -eq 3 ]
         if [ $argv[3] -eq $argv[3] ]  # test whether argument is integer
-            ag -i $argv[1]'.{1,'$argv[3]'}'$argv[2]
+            ag -i $argv[1]'.{1,'$argv[3]'}'$argv[2]'|'$argv[2]'.{1,'$argv[3]'}'
         end
     else if [ (count $argv) -eq 2 ]
-        ag -i $argv[1]'.{1,10}'$argv[2]
+        ag -i $argv[1]'.{1,10}'$argv[2]'|'$argv[2]'.{1,10}'$argv[1]
     else
         echo 'searches for 2 patterns near each other.'
         echo 'usage: '
