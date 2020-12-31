9940c29c8b2908ad1a86174b894ebf51b2357267
William Kong <coreloop09@gmail.com>
2020-12-31 15:59:29 -0500

    update fd

--- .config/fish/functions/fd.fish
+++ .config/fish/functions/fd.fish
@@ -1,5 +1,8 @@
 function fd --wraps=fdfind --description 'alias fd=fdfind'
   if test (uname) = "Linux"
     fdfind  $argv;
+  else
+    /usr/local/bin/fd $argv;
   end
 end
+
--- .config/fish/functions/ffind.fish
+++
@@ -1,4 +0,0 @@
-function ffind --wraps='find . -iname' --description 'alias ffind find . -iname'
-  find . -iname $argv;
-end
-
