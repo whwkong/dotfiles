8a465efa4945536baba3159ec4c297746d8150d8
William Kong <coreloop09@gmail.com>
2021-01-31 10:41:27 -0500

    update functions/config.fish

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,3 @@
-function config --description 'alias config=/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv;
+function config --description 'alias config=/usr/bin/env git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+	/usr/bin/env git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv;
 end
