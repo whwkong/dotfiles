1caace2c014e34d25e032843c32754e692d03bf2
William Kong <william@bht.ai>
2021-05-06 14:08:32 -0400

    update

--- .config/fish/functions/chrome.fish
+++ .config/fish/functions/chrome.fish
@@ -1,3 +1,4 @@
 function chrome --description 'alias chrome = open -a /Applications/Google\ Chrome.app/'
+    # linux: google-chrome $argv[1]
 	open -a /Applications/Google\ Chrome.app/ $argv[1]
 end
